1e6d7189b4741fb9358d69d0dfbddb2bd36cf4ec
Matt <mmszek3000@gmail.com>
2018-03-16 18:08:48 -0400

    .toLowerCase()

--- types/hero.js
+++ types/hero.js
@@ -7,8 +7,8 @@ module.exports = class HeroArgumentType extends ArgumentType {
   }
 
   validate (value) {
-    let name = value.split(':')[0].trim()
-    let title = (value.split(':')[1] || '').trim()
+    let name = value.split(':')[0].trim().toLowerCase()
+    let title = (value.split(':')[1] || '').trim().toLowerCase()
     let info = heroes.filter(hero => {
       let neededName = hero.name.replace(/ (.*)/, '')
       if (!title) return neededName === name
