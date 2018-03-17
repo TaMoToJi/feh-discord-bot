39524314f77c278912ddff67f7732eee005d36dc
Matt <mmszek3000@gmail.com>
2018-03-17 07:37:25 -0400

    Make info nice

--- commands/feh/info.js
+++ commands/feh/info.js
@@ -22,16 +22,44 @@ module.exports = class InfoCommand extends Command {
   }
 
   run (message, args) {
-    let hero = args.hero
-    let embed = new RichEmbed()
+    var hero = args.hero
+    var embed = new RichEmbed()
     embed
       .setThumbnail(encodeURI(hero.assets.portrait['113px']))
       .setTitle(`Hero Info: ${hero.name}`)
-      .addField('Rarities', hero.rarities)
-      .addField('Move Type', hero.moveType)
-      .addField('Weapon Type', hero.weaponType)
-    message.reply('', {
-      embed
-    })
+      .addField('Rarities', hero.rarities, true)
+      .addField('Move Type', hero.moveType, true)
+      .addField('Weapon Type', hero.weaponType, true)
+      .setFooter(
+        'Level 1 stats do not take variance into account. Level 40 stats do.'
+      )
+    for (let stars in hero.stats[1]) {
+      let stats = hero.stats[1][stars]
+      embed.addField(
+        `Level 1 / ${stars}\u2605`,
+        `HP: ${stats.hp}, Atk: ${stats.atk}, Spd: ${stats.spd}, Def: ${
+          stats.def
+        }, Res: ${stats.res}`
+      )
+    }
+    for (let stars in hero.stats[40]) {
+      let stats = hero.stats[40][stars]
+      let displayStats = []
+      for (let i = 0; i < 5; i++) {
+        let stat = stats[['hp', 'atk', 'spd', 'def', 'res'][i]]
+        for (let i = 0; i < 3; i++) {
+          let statNo = stat[i]
+          if (statNo === '-') stat.splice(i, 1)
+        }
+        displayStats.push(`${stat[0]}-${stat[stat.length - 1]}`)
+      }
+      embed.addField(
+        `Level 40 / ${stars}\u2605`,
+        `HP: ${displayStats[0]}, Atk: ${displayStats[1]}, Spd: ${
+          displayStats[2]
+        }, Def: ${displayStats[3]}, Res: ${displayStats[4]}`
+      )
+    }
+    message.reply('', { embed })
   }
 }
