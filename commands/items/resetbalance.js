d977df1502c7ce33f7e7ad0e1d0d127de9ddb007
ev3commander <ev3commander@users.noreply.github.com>
2018-03-18 15:56:42 -0400

    Added ResetBalance Cmd
    Oogoof

---
+++ commands/items/resetbalance.js
@@ -0,0 +1,43 @@
+const { Command } = require('discord.js-commando')
+const fs = require('fs')
+
+module.exports = class ResetBalanceCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'resetbalance',
+      group: 'items',
+      memberName: 'resetbalance',
+      description: "Resets a user's balance",
+      examples: ['resetbalance ev3commander', 'resetbalance MegaNoob123'],
+      aliases: ['resetbal', 'reset-balance', 'reset-bal', 'resetorbs', 'reset-orbs', 'clearbal', 'clear-balance', 'clearbalance', 'clear-bal', clearorbs', 'clear-orbs'],
+      args: [
+        {
+          key: 'user',
+          type: 'user',
+          prompt: 'Whose orbs do you want to reset?'
+        }
+      ],
+      ownerOnly: true
+    })
+  }
+  run (message, args) {
+    var database = JSON.parse(
+      fs.readFileSync('data.json', { encoding: 'utf-8' })
+    )
+    if (!database.users[args.user.id]) {
+      return message.reply(
+        `That user didn't start the game! They need to type \`@${
+          this.client.user.tag
+        } start\`.`
+      )
+    } else {
+      database.users[args.user.id].balance = 0;
+      fs.writeFile('data.json', JSON.stringify(database), err => {
+        if (err) throw err
+      })
+      return message.channel.send(
+        `Successfully reset ${args.user}'s orbs`
+      )
+    }
+  }
+}
