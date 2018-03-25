4358aa5d6cafc6b642c5880d0b48c4a89934c76a
Matt <mmszek3000@gmail.com>
2018-03-25 12:02:35 -0400

    test something

--- spam_git_pull.js
+++ spam_git_pull.js
@@ -5,14 +5,20 @@ const { webhook: config } = require('./config')
 const hook = new WebhookClient(config.id, config.token)
 
 setInterval(() => {
-  exec('git pull', (error, stdout) => {
-    if (error) console.log(error)
-    if (/Already up to date\./.test(stdout)) return
-    hook.send(
-      new RichEmbed()
-        .setTitle('Pulled changes via git')
-        .setDescription(`\`\`\`${stdout}\`\`\``)
-        .setColor('ORANGE')
-    )
-  })
+  exec(
+    'git pull',
+    {
+      detached: true
+    },
+    (error, stdout) => {
+      if (error) console.log(error)
+      if (/Already up to date\./.test(stdout)) return
+      hook.send(
+        new RichEmbed()
+          .setTitle('Pulled changes via git')
+          .setDescription(`\`\`\`${stdout}\`\`\``)
+          .setColor('ORANGE')
+      )
+    }
+  )
 }, 15e3)
