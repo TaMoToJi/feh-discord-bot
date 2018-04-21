6ad8799322166c40fd3588376bfd289b63f33193
Matt <mmszek3000@gmail.com>
2018-04-21 08:30:57 -0400

    proper ratelimits

--- commands/game/reset.js
+++ commands/game/reset.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord.js-commando')
 const fs = require('fs')
 var awaitConfirm = []
+var lastResets = {}
 
 module.exports = class ResetCommand extends Command {
   constructor (client) {
@@ -25,6 +26,9 @@ module.exports = class ResetCommand extends Command {
       return message.reply(
         `You didn't start the game! Try \`@${this.client.user.tag} start\`.`
       )
+    } else if ((new Date() - (lastResets[message.author.id] || new Date(0))) < 1000 * 60 * 60 * 2) {
+      let time = new Date(new Date() - lastResets[message.author.id])
+      return message.reply(`You can only reset once every 2 hours. Try again in ${-time.getUTCHours() + 1} hours, ${-time.getUTCMinutes() + 59} minutes, and ${-time.getUTCSeconds() + 59} seconds`)
     } else {
       if (!awaitConfirm[message.author.id]) {
         awaitConfirm[message.author.id] = true
@@ -39,6 +43,7 @@ module.exports = class ResetCommand extends Command {
         fs.writeFile('data.json', JSON.stringify(database), err => {
           if (err) throw err
         })
+        lastResets[message.author.id] = new Date()
         awaitConfirm[message.author.id] = false
         return message.channel.send(
           `Successfully reset the game for ${message.author}.`
