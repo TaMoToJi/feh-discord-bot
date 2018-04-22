d7dcca7a483a4a2eede7531a4bc577e39db8543c
Matt <mmszek3000@gmail.com>
2018-04-22 12:23:24 -0400

    stats command

---
+++ commands/util/stats.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord.js-commando')
+const { MessageEmbed } = require('discord.js')
+const fs = require('fs')
+
+module.exports = class StatsCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'botstats',
+      group: 'misc',
+      memberName: 'stats',
+      description: 'View the stats of the bot',
+      examples: ['stats'],
+      aliases: ['bot-stats']
+    })
+  }
+  async run (message) {
+    var database = JSON.parse(
+      fs.readFileSync('data.json', { encoding: 'utf-8' })
+    )
+    message.reply('', new MessageEmbed().setTitle('FEH Bot Stats').addField('Server Count', this.client.guilds.size).addField('Player Count', Object.keys(database.users).length).addField('Upvote Count', await this.client.dbl.getStats(this.client.user.id).points))
+  }
+}
